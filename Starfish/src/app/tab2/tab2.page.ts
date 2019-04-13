95d463a0c3973fb4444eb34aa0c8069df7cfb033
elenalape <elena@hotmail.lt>
2019-04-13 17:54:28 +0100

    add camera, not uploading to db yet

--- Starfish/src/app/tab2/tab2.page.ts
+++ Starfish/src/app/tab2/tab2.page.ts
@@ -1,8 +1,62 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
 import { Component } from '@angular/core';
+import { Camera } from '@ionic-native/camera/ngx';
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+
+	beforephoto: any;
+	afterphoto: any;
+	clicked: boolean;
+	public post1: any = {color: 'primary'};
+	public post2: any = {color: 'primary'};
+
+	constructor(public cameraPlugin: Camera) {};
+
+	takeBeforePicture(): void {
+	this.post1.color = 'light';
+	this.cameraPlugin.getPicture({
+    quality : 95,
+    targetWidth: 300,
+    targetHeight: 300,
+    destinationType : this.cameraPlugin.DestinationType.DATA_URL,
+    sourceType : this.cameraPlugin.PictureSourceType.CAMERA,
+    encodingType: this.cameraPlugin.EncodingType.JPEG,
+    mediaType: this.cameraPlugin.MediaType.PICTURE,
+		
+	  }).then((imageData) => {
+	  	this.beforephoto = 'data:image/jpeg;base64,' + imageData;
+	  }, error => {
+	  	console.log("oops");
+	  });
+};
+
+	takeAfterPicture(): void {
+	this.post2.color = 'light';
+	this.cameraPlugin.getPicture({
+    quality : 95,
+    targetWidth: 300,
+    targetHeight: 300,
+    destinationType : this.cameraPlugin.DestinationType.DATA_URL,
+    sourceType : this.cameraPlugin.PictureSourceType.CAMERA,
+    encodingType: this.cameraPlugin.EncodingType.JPEG,
+    mediaType: this.cameraPlugin.MediaType.PICTURE,
+		
+	  }).then((imageData) => {
+	  	this.afterphoto = 'data:image/jpeg;base64,' + imageData;
+	  	this.clicked = true;
+
+	  }, error => {
+	  	console.log("oops");
+	  });
+}
+
+}
