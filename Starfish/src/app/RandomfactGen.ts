bdecb225a703ada6ce91375c0a09555348829f5d
Anna <s1601847@sms.ed.ac.uk>
2019-04-13 18:08:20 +0100

    adding random fact generator .ts

---
+++ Starfish/src/app/RandomfactGen.ts
@@ -0,0 +1,14 @@
+ * @param {Number} min 
+ * @param {Number} max
+ * @return {Number} random generated integer 
+ */
+ randomInt(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+ }
+ var Facts = new Array<String>("100,000 marine mammals and turtles and 1 million sea birds are killed by marine plastic pollution annually.","Sea animals often mistake plastic for food and will ingest plastic. It fills up their stomachs and they feel full even though they’re starving to death.","Seabirds can become snared in plastic, leading to injury and often death.","To dolphins and seals, plastic can seem like toys, but they can often get tangled in it and suffocate.", "Plastic can be broken down into little bits and be consumed by fish and other animals. When we eat these fish or other seafood, we are also eating that plastic they’ve ingested.", "Recycling leads to less toxic chemicals released into the air.", "It takes 5000 years for a plastic water bottle to fully decompose.", "Plastic waste suffocates the ground and makes it hard for plants to grow", "There is more microplastic in the ocean then there are stars in the milky way galaxy", "Almost every piece of plastic ever created still exists today", "Small animals can get their heads stuck in plastic bottles and cans that aren’t disposed of properly", "Around 50% of the plastic that people use are used only once.", "Over the last ten years we have produced more plastic than during the whole of the last century.");
+ 
+ randfact{
+     let rd = randomInt(Facts.length)
+     let fct = Facts[rd]
+     return fct
+ }
